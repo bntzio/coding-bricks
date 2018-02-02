1ea28636770c84f473e1e0376340d6e4cc6f2461
Enrique Benitez <enriquebenitezjr@gmail.com>
2018-02-02 16:57:38 -0600

    Change text of button on hover

--- src/javascripts/modules/animations.js
+++ src/javascripts/modules/animations.js
@@ -2,6 +2,7 @@ const $ = require('jquery')
 const { TimelineMax, TweenMax } = require('gsap')
 
 $(document).ready(() => {
+  /* gsap */
   const $char = $('.top__title__char')
   const tl = new TimelineMax({ repeat: -1, repeatDelay: 10 })
   tl.staggerTo($char, 0.2, { scale: 1.2, y: 10, repeat: 1, yoyo: true }, 0.1)
@@ -18,5 +19,12 @@ $(document).ready(() => {
   })
   $char.mouseleave((el) => {
     TweenMax.to(el.currentTarget, 0.3, { scale: 1, rotation: 0 })
+  })
+  /* jquery */
+  $('#enroll').mouseenter(function () {
+    $(this).text('Enroll! 🎉')
+  })
+  $('#enroll').mouseleave(function () {
+    $(this).text('Enroll')
   })
 })
